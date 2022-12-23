5acde245cbc3897bdaf0389713c032ab0d5059c8
Chaitalimane2027 <114143495+Chaitalimane2027@users.noreply.github.com>
2022-12-23 15:39:18 +0530

    Update jwt.controller.js

--- src/controllers/jwt.controller.js
+++ src/controllers/jwt.controller.js
@@ -6,10 +6,16 @@ exports.generateToken = (userDetails)=>{
 
     return jwt.sign(userDetails,jwtSecretKey,
         {
-            expiresIn: '5m'
+            expiresIn: '20m'
           });
 
 }
 exports.validateToken = (token) =>{
-    return jwt.verify(token,jwtSecretKey);
-}
+//     const isValid =  jwt.verify(token,jwtSecretKey);
+//     if(isValid){
+//         return jwt.decode(token,jwtSecretKey);
+//     }
+   return jwt.verify(token,jwtSecretKey);
+ }
+
+
